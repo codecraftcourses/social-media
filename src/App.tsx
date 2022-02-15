dc440ccfb70ddf6c32fd96f2fce022edb851ae4b
codecraftcourses <codecraftcourses@outlook.com>
2022-02-15 17:43:14 +0000

    feat: add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  return <main>App</main>;
+  return (
+    <main>
+      <Outlet />
+    </main>
+  );
 }
 
 export default App;
